ae11257c889afad91e0f9cd2df4fc8fb86428edb
Panos Dask <panosdaskalopoulos259@gmail.com>
2023-01-19 19:39:31 +0200

    Split Modules in to 3 types for better usage depending on circumstance

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
 """
 This module defines some constant names. These are Percentiles of grades and threshold for different checks
 """
-MODULES = ['README', 'BIG_README', 'README_USES_MARKDOWN', 'BUILD_EXISTS', 'BUILD_FILE_OK', 'LICENCE_FILE']
+TOP_MODULES = ['README', 'PACKAGING']
+BONUS_MODULES = ['BIG_README', 'README_USES_MARKDOWN', 'LICENCE_FILE']
+FINAL_MODULES = ['README', 'BIG_README', 'README_USES_MARKDOWN', 'BUILD_EXISTS', 'BUILD_FILE_OK', 'LICENCE_FILE']
 # Percentiles of each assignment
 CHECKSTYLE = 0.15
 TESTING = 0.25
@@ -36,11 +38,11 @@ METHODS_PER_COMMENT = 1
 # TODO check if this is something we would like. Otherwise flag for removal.
 #   This could change year on year to not be taken advantage of.
 # Bonus points out of 10 for
-LICENCE_FILE = 0.1
-CONTRIBUTING_FILE = 0.1
-BIG_README = 0.1
-README_USES_MARKDOWN = 0.1  # it's not a txt, and it has used at least a couple of markdown elements
-GITHUB_FEATURES = 0.1  # Issues, actions, wiki , projects
+LICENCE_FILE = 0.01
+CONTRIBUTING_FILE = 0.01
+BIG_README = 0.01
+README_USES_MARKDOWN = 0.01  # it's not a txt, and it has used at least a couple of markdown elements
+GITHUB_FEATURES = 0.01  # Issues, actions, wiki , projects
 
 # Bonus Thresholds
 BIG_README_SIZE = 1500
