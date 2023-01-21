1ed733a34e378b04c4bbc3ba87e79b83e72b5d5f
Panos Dask <panosdaskalopoulos259@gmail.com>
2023-01-21 18:33:09 +0200

    new Search method for files which incorporates exclusions

--- search.py
+++ search.py
@@ -1,6 +1,11 @@
 """
 This module defines all methods which deal with finding files
 """
+import os
+
+from github import Github
+
+g = Github(os.environ['GITHUB_GPG_KEY'])
 
 
 # Looks for names containing a specific element. For example .java
@@ -17,6 +22,25 @@ def search_name_contains_return_size(name_contains, repo):
                 # get the name and size of the file
                 file_sizes[content.name] = content.size
         return file_sizes
+
+    return search_directory('')
+
+
+# Looks for names containing a specific element. For example .java
+# Returns files in dict with content
+def search_name_contains_return_file(name_contains, name_doesnt_contain, repo_address):
+    # recursively searching all the directories
+    repo = g.get_repo(repo_address)
+    def search_directory(directory):
+        contents = repo.get_contents(directory)
+        files = {}
+        for content in contents:
+            if content.type == "dir":
+                files.update(search_directory(content.path))
+            elif name_contains in content.name and name_doesnt_contain not in content.name:
+                # get the name and contents
+                files[content.name] = content.decoded_content.decode('utf-8')
+        return files
 
     return search_directory('')
 
