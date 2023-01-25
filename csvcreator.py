a90dc54010f293f7e09bd9e9df09f997ae03f857
Panos Dask <panosdaskalopoulos259@gmail.com>
2023-01-25 17:14:26 +0200

    New module which deals with a CSV creation

---
+++ csvcreator.py
@@ -0,0 +1,32 @@
+"""
+This module deals with the creation of a raw CSV for later grading (externally)
+"""
+import csv
+
+
+def initialise(headers):
+    """
+    This function is used to create and write the headers of a new CSV file
+
+    :param headers: List of strings representing the headers of the CSV file
+    :type headers: list of str
+    :return: None
+    :rtype: None
+    """
+    with open('./results/result.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+
+
+def add(data):
+    """
+       This function is used to add a row of data to an existing CSV file
+
+       :param data: List of strings representing the data to be added to the CSV file
+       :type data: list of str
+       :return: None
+       :rtype: None
+    """
+    with open('./results/result.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
